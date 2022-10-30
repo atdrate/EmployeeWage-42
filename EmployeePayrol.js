644a49a9a57877afb7e425d727726c206eb959d5
subodhNagalwade <subodhnagalwade@gmail.com>
2022-10-30 12:26:59 +0530

    Check EMployee present for full time or half time

--- EmployeePayrol.js
+++ EmployeePayrol.js
@@ -1,4 +1,4 @@
-const Is_ABSENT = 0
+/*const Is_ABSENT = 0
 let empCheck = Math.floor(Math.random() * 10) %2;
 if(empCheck == Is_ABSENT)
 {
@@ -6,4 +6,31 @@ if(empCheck == Is_ABSENT)
     return;
 }else{
     console.log("UC1 - Employee is present");
+}*/
+//uc2
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=2;
+ 
+let empHrs=0;
+ 
+let empCheck=Math.floor(Math.random()* 10 % 3);
+
+switch(empCheck)
+{
+ case IS_PART_TIME:
+      empHrs=PART_TIME_HOURS;
+      break;
+case IS_FULL_TIME:
+      empHrs=FULL_TIME_HOURS;
+      break;
+default:
+      empHrs=0;
+ 
 }
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("UC-2 Emp wage: "+empWage);
+
